0eed3383758b0fc42888f52692dc44cb7f7c3843
Larisa Novozhilova <kara4516@gmail.com>
2024-04-15 11:40:49 +0300

    feat: solved 3 tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -114,8 +114,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cached = new Map();
+  return (arg) => {
+    if (cached.has(arg)) {
+      return cached.get(arg);
+    }
+    const result = func(arg);
+    cached.set(arg, result);
+    return result;
+  };
 }
 
 /**
@@ -177,8 +185,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -198,8 +208,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom - 1;
+  return () => {
+    count += 1;
+    return count;
+  };
 }
 
 module.exports = {
